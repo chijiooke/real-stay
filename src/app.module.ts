5509867d2d1b8e8f8b4cc84049a6dfdebaec0bdb
chijioke <michaelsylva36@gmail.com>
2025-04-01 23:15:25 +0100

    feat(render): render yaml file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { ListingModule } from './listing/listing.module';
 
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/';
+const DB_NAME = process.env.DB_NAME || 'real-stay';
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/real-stay'),
+    MongooseModule.forRoot(MONGO_URI + DB_NAME),
     AuthModule,
     UsersModule,
     ListingModule,
--- src/main.ts
+++ src/main.ts
@@ -14,8 +14,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors(); // Enable CORS if needed
-  await app.listen(3000); // Run on port 3000
-  
+  await app.listen(process.env.PORT || '3000'); 
 }
 
 // Call bootstrap to initialize the app
@@ -23,4 +22,3 @@ bootstrap();
 
 // Export the Firebase function
 export const api = functions.https.onRequest(server);
-
