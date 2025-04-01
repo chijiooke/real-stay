ec1beebc5c5c5d97d06c1171a939cc7a7dc51887
chijioke <michaelsylva36@gmail.com>
2025-04-01 23:34:30 +0100

    fix(redis): redis config

--- src/utility-services/redis.ts
+++ src/utility-services/redis.ts
@@ -7,8 +7,9 @@ export class RedisService {
 
   constructor() {
     this.client = new Redis({
-      host: process.env.REDIS_HOST || '',
-      port: Number(process.env.REDIS_PORT),
+      host: process.env.REDIS_HOST || '127.0.0.1',
+      port: Number(process.env.REDIS_PORT) || 6379,
+      password: process.env.REDIS_PASSWORD || '',
     });
 
     this.client.on('error', (err) => {
